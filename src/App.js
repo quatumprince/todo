3df3aed84b02c2d79630853ead3556bb7387fa79
Prince <52575917+quatumprince@users.noreply.github.com>
2023-04-28 13:34:09 +0530

    changed addNew task and complete functiom

--- src/App.js
+++ src/App.js
@@ -4,92 +4,96 @@ import { useAccount, useContract, useSigner } from "wagmi";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./contract";
 
 function App() {
-	const [tasks, setTasks] = useState([]);
-  const [inputTask, setInputTask] = useState(null)
+  const [tasks, setTasks] = useState([]);
+  const [inputTask, setInputTask] = useState(null);
 
-  // console.log("INPUT ", inputTask)
+  const { address } = useAccount();
+  const { data: signer } = useSigner();
+  const contract = useContract({
+    address: CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    signerOrProvider: signer,
+  });
 
-	const { address } = useAccount();
-	const { data: signer } = useSigner();
-	const contract = useContract({
-		address: CONTRACT_ADDRESS,
-		abi: CONTRACT_ABI,
-		signerOrProvider: signer,
-	});
+  console.log("CONTRACT ", contract);
 
-	console.log("CONTRACT ", contract);
-
-	const getTasks = async () => {
-		try {
-			let tasks = [];
-			let tasks_count = await contract.count();
-			console.log("TOTAL NUMBER OF TASKS ", tasks_count.toString());
-			if (tasks_count) {
-				tasks_count = +tasks_count;
-				// Fetch tasks
-				for (let i = 0; i < tasks_count; i++) {
-					const task = await contract.tasks(i);
-					if (task) {
-						tasks.push(task);
-					}
-				}
-			}
-			// Set task in the state
-			setTasks(tasks);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-  const addNewTask = async() => {
+  const getTasks = async () => {
     try {
-      const newTask = await contract.addTask(inputTask)
-      console.log(newTask)
-      await newTask.wait()
-      console.log(newTask)
-      setTasks(prev => [...prev, [inputTask, false]])
-      setInputTask(null)
-      document.getElementById('input-task').value = ""
-    } catch(err) {
-      console.log(err)
+      let tasks = [];
+      let tasks_count = await contract.count();
+      console.log("TOTAL NUMBER OF TASKS ", tasks_count.toString());
+      if (tasks_count) {
+        tasks_count = +tasks_count;
+        // Fetch tasks
+        for (let i = 0; i < tasks_count; i++) {
+          const task = await contract.tasks(i);
+          if (task) {
+            tasks.push(task);
+          }
+        }
+      }
+      // Set task in the state
+      setTasks(tasks);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
+  const addNewTask = async () => {
+    try {
+      const newTask = await contract.addTask(inputTask);
+      console.log(newTask);
+      await newTask.wait();
+      setTasks((prev) => [...prev, [inputTask, false]]);
+      setInputTask(null);
+      document.getElementById("input-task").value = "";
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const completeTask = async (task, event) => {
+	try {
+	  const taskIndex = tasks.findIndex((a) => a[0] === task);
+	  if (tasks[taskIndex][1]) {
+		console.log("Task already completed");
+		return;
+	  }
+	  const completeTask = await contract.completeTask(taskIndex, { gasLimit: 100000 });
+	  await completeTask.wait();
+	  const updatedTasks = tasks.map((taskItem, i) => {
+		if (i === taskIndex) {
+		  return [taskItem[0], true];
+		} else {
+		  return taskItem;
+		}
+	  });
+	  setTasks(updatedTasks);
+	} catch (err) {
+	  console.log(err);
+	}
+  };
   
-  const completeTask = async(task) => {
-    try {
-      console.log(tasks)
-      const taskIndex = tasks.findIndex(a => a[0] === task)
-      console.log("index ", taskIndex)
-      const completeTask = await contract.completeTask(taskIndex)
-      console.log(completeTask)
-      await completeTask.wait()
-      const newTasks = tasks.filter(a => a[0] !== task)
-      console.log(newTasks)
-      setTasks(newTasks)
-    } catch(err) {
-      console.log(err)
+  
+
+  useEffect(() => {
+    if (contract) {
+      getTasks();
     }
-  }
+  }, [contract]);
 
-	useEffect(() => {
-		if (contract) {
-			getTasks();
-		}
-	}, [contract]);
-
-	return (
-		<div className="flex flex-col justify-center items-center bg-black text-white">
-			<div className="flex items-center justify-between w-full px-4 py-2">
-				<p className="text-xl font-bold">Todo-List</p>
-				{address && <ConnectButton />}
-			</div>
-			<div
-				style={{ minHeight: "95vh" }}
-				className="flex flex-col items-center justify-center gap-4 w-full"
-			>
-				<h1 className="text-4xl font-extrabold">Todo List</h1>
-				{!address && <ConnectButton />}
+  return (
+    <div className="flex flex-col justify-center items-center bg-black text-white">
+      <div className="flex items-center justify-between w-full px-4 py-2">
+        <p className="text-xl font-bold">Todo-List</p>
+        {address && <ConnectButton />}
+      </div>
+      <div
+        style={{ minHeight: "95vh" }}
+        className="flex flex-col items-center justify-center gap-4 w-full"
+      >
+        <h1 className="text-4xl font-extrabold">Todo List</h1>
+        {!address && <ConnectButton />}
 
         {/* Add Task */}
         <div className="flex flex-row items-center justify-center gap-4">
